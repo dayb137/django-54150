5874cd712a4c3bf191c66c99fedf416b2253fc80
day <dayblas007@gmail.com>
2024-06-11 18:03:00 -0300

    Clase 21 - Playground Intermedio Parte 3

--- inicio/models.py
+++ inicio/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Auto(models.Model):
     modelo = models.CharField(max_length=20)
     marca = models.CharField(max_length=20)
+    # anio = models.IntegerField(default=2010)
     
     def __str__(self) -> str:
         return f'Soy el auto {self.modelo} {self.marca}'
--- inicio/views.py
+++ inicio/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime
 
 from django.http import HttpResponse
 from django.template import Template, Context, loader
 
 from inicio.models import Auto
-
+# from inicio.models import Auto
+from inicio.forms import CrearAutoFormulario
 import random
 
 
@@ -110,3 +111,38 @@ def crear_auto(request, marca, modelo):
      auto.save()
      return render(request, 'auto_templates/creacion.html', {'auto':auto})
  
+def crear_auto_v2(request):
+    #V!
+    # print('Valor de la request: ', request)
+    # print('Valor de GET', request.GET)
+    # print('Valor POST', request.POST)
+    
+    # # request.POST
+    # if request.method == 'POST':
+    #     auto = Auto(marca=request.POST.get('marca'), modelo=request.POST.get('modelo'))
+    #     auto.save()
+        
+    # return render(request, 'inicio/crear_auto_v2.html')
+    print('Valor de la request: ', request)
+    print('Valor de GET', request.GET)
+    print('Valor POST', request.POST)
+    
+    formulario = CrearAutoFormulario()
+    
+    if request.method == 'POST':
+        formulario = CrearAutoFormulario(request.POST)
+        if formulario.is_valid():
+           datos = formulario.cleaned_data
+           auto = Auto(marca=datos.get('marca'), modelo=datos.get('modelo'))
+           auto.save()    
+           return redirect('autos')
+        
+        
+                           
+    return render(request, 'inicio/crear_auto_v2.html', {'formulario': formulario})
+
+def autos(request):
+    
+    autos = Auto.objects.all()
+    
+    return render(request, 'inicio/autos.html', {'autos':autos})
